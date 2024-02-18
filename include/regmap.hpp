a183bb0c1fe5097677da02599e1ce675a92f15d4
ZhuWei <wei2.zhu@intel.com>
2024-02-18 09:39:25 -0500

    add tile load/store

--- include/regmap.hpp
+++ include/regmap.hpp
@@ -108,6 +108,22 @@ struct AddressPayload {
   inline const uint32_t& getPayload() const {
     return payloadReg_;
   }
+  
+  inline AddressPayload& UpdateSrc0AddrX(int offset) {
+    asm volatile (
+        "mov (M1, 1) %0(0, 5)<1> %1(0, 0)<0;1,0> \n"
+        : "+rw"(payloadReg_) : "rw"(offset)
+    );
+    return *this;
+  }
+
+  inline AddressPayload& UpdateSrc0AddrY(int offset) {
+    asm volatile (
+        "mov (M1, 1) %0(0, 6)<1> %1(0, 0)<0;1,0> \n"
+        : "+rw"(payloadReg_) : "rw"(offset)
+    );
+    return *this;
+  }  
 
   inline AddressPayload& addSrc0AddrX(int offset) {
     asm volatile (
@@ -267,7 +283,7 @@ struct __Matrix {
 
   static_assert(sizeof(sycl::vec<T, N>)/sizeof(uint32_t) == NumRegs);
   using rawType = sycl::vec<uint32_t, sizeof(sycl::vec<T, N>)/sizeof(uint32_t)>;
-
+  using storage_type = typename sycl::vec<T, N>::vector_t;
   inline typename sycl::vec<T, N>::vector_t& getStorage() {
     return reinterpret_cast<typename sycl::vec<T, N>::vector_t&>(registerImage_);
   }
@@ -400,10 +416,10 @@ struct __ArrayMatrix {
 
   typedef __attribute__((ext_vector_type(N))) T storage_type;
   inline storage_type& getStorage() {
-    return reinterpret_cast<storage_type&>(registerImage_);
+    return registerImage_;
   }
   inline const storage_type& getStorage() const {
-    return reinterpret_cast<const storage_type&>(registerImage_);
+    return registerImage_;
   }
 
   __ArrayMatrix() = default;
---
+++ include/tile_load_store.hpp
@@ -0,0 +1,114 @@
+#include "lsc.hpp"
+#include "regmap.hpp"
+
+// #define __SYCL_DEVICE_ONLY__
+
+template <typename T, int TileHeight, int TileWidth, DataShuffle Transpose,
+          int SubGroupSize = 16>
+struct TileLoad;
+
+template <typename T, int TileHeight, int TileWidth, DataShuffle Transpose,
+          int SubGroupSize = 16>
+struct TileStore;
+
+#if defined(__SYCL_DEVICE_ONLY__)
+template <typename T, int TileHeight, int TileWidth, int SubGroupSize>
+struct TileLoad<T, TileHeight, TileWidth, DataShuffle::none, SubGroupSize> {
+  // TODO: InnerLayout requries bytes for width, how to make it easier
+  static constexpr int N = InnerLayout<T, TileHeight, TileWidth * sizeof(T),
+                                       DataShuffle::none, SubGroupSize>::N;
+  // TODO: no necessary to load maximum for every time
+  static constexpr int max_load_height_in_elem = 16;
+  static constexpr int max_load_width_in_bytes = 32;
+  static constexpr int default_load_height_in_elem = 8;
+  static constexpr int default_load_width_in_bytes = 32;
+  static constexpr int load_height_in_elem =
+      max_load_height_in_elem > TileHeight ? TileHeight
+                                           : max_load_height_in_elem;
+  static constexpr int load_width_in_bytes = max_load_width_in_bytes >
+                                                     TileWidth * sizeof(T)
+                                                 ? TileWidth * sizeof(T)
+                                                 : max_load_width_in_bytes;
+
+  static_assert(TileWidth * sizeof(T) % load_width_in_bytes == 0,
+                "tile width must be a multiple of load_width_in_bytes");
+  static_assert(TileHeight % load_height_in_elem == 0,
+                "tile height must be a multiple of load_height_in_elem");
+  static constexpr int num_block_h = TileHeight / load_height_in_elem;
+  static constexpr int num_block_w =
+      TileWidth * sizeof(T) / load_width_in_bytes;
+  using matrix_type = __ArrayMatrix<T, load_height_in_elem, load_width_in_bytes,
+                                    DataShuffle::none, SubGroupSize>;
+  using storage_type = typename matrix_type::storage_type;                                    
+  using address_payload_type =
+      AddressPayload<load_height_in_elem, load_width_in_bytes>;
+  static constexpr int num_elem = N / matrix_type::N;
+  static_assert(num_elem == num_block_h * num_block_w, "load with tile is not supported currently");
+  inline void operator()(T *SurfaceBase, uint32_t SurfaceHeight, uint32_t SurfaceWidth,
+                  uint32_t SurfacePitch, int Src0AddrX, int Src0AddrY,
+                  storage_type (& tile)[num_elem]) {
+    address_payload_type address_payload(SurfaceBase, SurfaceHeight,
+                                         SurfaceWidth, SurfacePitch, Src0AddrX,
+                                         Src0AddrY);
+
+    for (int i = 0; i < num_block_h; ++i) {
+      address_payload.UpdateSrc0AddrY(Src0AddrY + i * load_height_in_elem);
+      for (int j = 0; j < num_block_w; ++j) {
+        // TODO: fix,
+        // 1. update payload,
+        // 2. remove copy from subtile to tile
+        address_payload.UpdateSrc0AddrX(Src0AddrX + j * load_width_in_bytes);
+        matrix_type sub_tile;
+        lscLoad(sub_tile, address_payload);
+        
+        tile[i * num_block_w + j] = std::move(sub_tile.getStorage());
+      }
+    }
+  }
+};
+
+template <typename T, int TileHeight, int TileWidth, int SubGroupSize>
+struct TileStore<T, TileHeight, TileWidth, DataShuffle::none, SubGroupSize> {
+  // TODO: InnerLayout requries bytes for width, how to make it easier
+  static constexpr int N = InnerLayout<T, TileHeight, TileWidth * sizeof(T),
+                                       DataShuffle::none, SubGroupSize>::N;
+  // TODO: 
+  static constexpr int max_store_height_in_elem = 8;
+  static constexpr int max_store_width_in_bytes = 32;
+  static constexpr int store_height_in_elem = 8;
+  static constexpr int store_width_in_bytes = 32;
+  static_assert(TileWidth * sizeof(T) % store_width_in_bytes == 0,
+                "tile width must be a multiple of store_width_in_bytes");
+  static_assert(TileHeight % store_height_in_elem == 0,
+                "tile height must be a multiple of store_height_in_elem");
+  static constexpr int num_block_h = TileHeight / store_height_in_elem;
+  static constexpr int num_block_w =
+      TileWidth * sizeof(T) / store_width_in_bytes;
+  using matrix_type =
+      __ArrayMatrix<T, store_height_in_elem, store_width_in_bytes,
+                    DataShuffle::none, SubGroupSize>;
+  using storage_type = typename matrix_type::storage_type;                         
+  using address_payload_type =
+      AddressPayload<store_height_in_elem, store_width_in_bytes>;
+  static constexpr int num_elem = N / matrix_type::N;
+  static_assert(num_elem == num_block_h * num_block_w, "store with tile is not supported currently");  
+  inline void operator()(T *SurfaceBase, uint32_t SurfaceHeight, uint32_t SurfaceWidth,
+                  uint32_t SurfacePitch, int Src0AddrX, int Src0AddrY,
+                 storage_type (& tile)[num_elem]) {
+    address_payload_type address_payload(SurfaceBase, SurfaceHeight,
+                                         SurfaceWidth, SurfacePitch, Src0AddrX,
+                                         Src0AddrY);
+    for (int i = 0; i < num_block_h; ++i) {
+      address_payload.UpdateSrc0AddrY(Src0AddrY + i * store_height_in_elem);
+      for (int j = 0; j < num_block_w; ++j) {
+        // TODO: fix,
+        // 1. update payload, not init every time
+        // 2. remove copy from subtile to tile
+        address_payload.UpdateSrc0AddrX(Src0AddrX + j * store_width_in_bytes);
+        matrix_type sub_tile(std::move(tile[i * num_block_w + j]));
+        lscStore(address_payload, sub_tile);
+      }
+    }
+  }
+};
+#endif
