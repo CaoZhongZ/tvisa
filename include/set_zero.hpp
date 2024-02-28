98ada7e4960d32ddaf89f1622f4ffbc72b9d5c45
Cao Zhong Z <zhong.z.cao@intel.com>
2024-02-28 12:10:51 +0800

    Try use null register
    Signed-off-by: Cao Zhong Z <zhong.z.cao@intel.com>

--- include/set_zero.hpp
+++ include/set_zero.hpp
@@ -7,37 +7,34 @@ template <typename T, int N> struct SetZero;
 #define CONCAT(A, B) _CONCAT(A, B)
 #define _CONCAT(A, B) A##B
 
-#define _MOV1 mov (M1, 16) P1(0, 0)<1> 0x0:ud\n
-#define _MOV2 _MOV1 mov (M1, 16) P1(1, 0)<1> 0x0:ud\n
-#define _MOV4                                                                  \
-  _MOV2 mov (M1, 16) P1(2, 0)<1> 0x0:ud\n                                 \
-        mov (M1, 16) P1(3, 0)<1> 0x0:ud\n
-#define _MOV8                                                                  \
-  _MOV4 mov (M1, 16) P1(4, 0)<1> 0x0:ud\n                                 \
-        mov (M1, 16) P1(5, 0)<1> 0x0:ud\n                                 \
-        mov (M1, 16) P1(6, 0)<1> 0x0:ud\n                                 \
-        mov (M1, 16) P1(7, 0)<1> 0x0:ud\n
-#define _MOV16                                                                 \
-  _MOV8 mov (M1, 16) P1(8, 0)<1> 0x0:ud\n                                 \
-        mov (M1, 16) P1(9, 0)<1> 0x0:ud\n                                 \
-        mov (M1, 16) P1(10, 0)<1> 0x0:ud\n                                \
-        mov (M1, 16) P1(11, 0)<1> 0x0:ud\n                                \
-        mov (M1, 16) P1(12, 0)<1> 0x0:ud\n                                \
-        mov (M1, 16) P1(13, 0)<1> 0x0:ud\n                                \
-        mov (M1, 16) P1(14, 0)<1> 0x0:ud\n                                \
-        mov (M1, 16) P1(15, 0)<1> 0x0:ud\n
+#define _MOV1 "mov (M1, 16) alias_(0, 0)<1> V0(0, 0)<1;1,0>\n"
+#define _MOV2 _MOV1 "mov (M1, 16) alias_(1, 0)<1> V0(0, 0)<1;1,0>\n"
 
-#define Stringify(A) _Stringify(A)
-#define _Stringify(A) #A
-#define MOV(num) Stringify(CONCAT(_MOV, num))
+#define _MOV4                                                   \
+  _MOV2 "mov (M1, 16) alias_(2, 0)<1> V0(0,0)<1;1,0>\n"          \
+        "mov (M1, 16) alias_(3, 0)<1> V0(0,0)<1;1,0>\n"
 
+#define _MOV8                                                   \
+  _MOV4 "mov (M1, 16) alias_(4, 0)<1> V0(0,0)<1;1,0>\n"          \
+        "mov (M1, 16) alias_(5, 0)<1> V0(0,0)<1;1,0>\n"          \
+        "mov (M1, 16) alias_(6, 0)<1> V0(0,0)<1;1,0>\n"          \
+        "mov (M1, 16) alias_(7, 0)<1> V0(0,0)<1;1,0>\n"
 
+#define _MOV16                                                  \
+  _MOV8 "mov (M1, 16) alias_(8, 0)<1> V0(0,0)<1;1,0>\n"          \
+        "mov (M1, 16) alias_(9, 0)<1> V0(0,0)<1;1,0>\n"          \
+        "mov (M1, 16) alias_(10, 0)<1> V0(0,0)<1;1,0>\n"         \
+        "mov (M1, 16) alias_(11, 0)<1> V0(0,0)<1;1,0>\n"         \
+        "mov (M1, 16) alias_(12, 0)<1> V0(0,0)<1;1,0>\n"         \
+        "mov (M1, 16) alias_(13, 0)<1> V0(0,0)<1;1,0>\n"         \
+        "mov (M1, 16) alias_(14, 0)<1> V0(0,0)<1;1,0>\n"         \
+        "mov (M1, 16) alias_(15, 0)<1> V0(0,0)<1;1,0>\n"
 
 template <typename T> struct SetZero<T, 4> {
   static inline void run(T &target) {
     asm volatile("{\n"
-                  ".decl P1 v_type=G type=ud num_elts=64 align=GRF alias=<%0, 0>\n" 
-                  MOV(4)
+                  ".decl alias_ v_type=G type=ud num_elts=64 align=GRF alias=<%0, 0>\n"
+                  _MOV4
                   "}\n"
                  : "=rw"(target));
   }
@@ -46,8 +43,8 @@ template <typename T> struct SetZero<T, 4> {
 template <typename T> struct SetZero<T, 8> {
   static inline void run(T &target) {
     asm volatile("{\n"
-                ".decl P1 v_type=G type=ud num_elts=128 align=GRF alias=<%0, 0>\n" 
-                  MOV(8)
+                ".decl alias_ v_type=G type=ud num_elts=128 align=GRF alias=<%0, 0>\n"
+                  _MOV8
                 "}\n"
                  : "=rw"(target));
   }
@@ -56,8 +53,8 @@ template <typename T> struct SetZero<T, 8> {
 template <typename T> struct SetZero<T, 16> {
   static inline void run(T &target) {
     asm volatile("{\n"
-                  ".decl P1 v_type=G type=ud num_elts=256 align=GRF alias=<%0, 0>\n" 
-                  MOV(16)
+                  ".decl alias_ v_type=G type=ud num_elts=256 align=GRF alias=<%0, 0>\n"
+                  _MOV16
                   "}\n"
                  : "=rw"(target));
   }
